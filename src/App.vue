92dec45f780900e63286fbd89e7453b844c61743
it27427 <istiak.tushar.qtec@gmail.com>
2022-12-28 17:27:14 +0600

    learning vuex

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h1>Test App</h1>
 </template>
-
+<!-- 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -27,4 +23,4 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
-</style>
+</style> -->
